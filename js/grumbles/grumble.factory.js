1c6ce178d753ce4cd3f9c058bc2feacdf9c0db37
Markham Shofner <markham.shofner@gmail.com>
2015-12-16 16:50:33 -0500

    index and show functionality

---
+++ js/grumbles/grumble.factory.js
@@ -0,0 +1,13 @@
+(function() {
+  angular
+    .module("grumbles")
+    .factory("GrumbleFactory", [
+      "$resource",
+      FactoryFunction
+    ]);
+
+    function FactoryFunction($resource){
+      return $resource("http://localhost:3000/grumbles/:id");
+    }
+
+})();
